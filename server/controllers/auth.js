806ba3281bb7fe20bafaeade8b95d807eb732f51
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-11-02 08:33:44 +0700

    start conect database with prisma

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,18 @@
 exports.register = async (req, res) => {
   //code for routs eiei
   try {
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400).json({ message: "Email is required!!!" });
+    }
+    if (!password) {
+      return res.status(400).json({ message: "Password is required!!!" });
+    }
+
+    //check email in database already ?
+
+    console.log(email);
+    console.log(password);
     res.send("Hello Register");
   } catch (err) {
     console.log(err);
