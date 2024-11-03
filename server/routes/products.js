63d44181264d335e5f7e0ab2eef8144d562d3e4c
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-11-03 08:36:00 +0700

    link controller with product routers

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const {
+  create,
+  list,
+  listby,
+  remove,
+  searchFilters,
+} = require("../controllers/products");
 
 // @ENDPOINT http://localhost:5000/api/product
-router.post("/product");
-router.get("/products/:count");
-router.delete("/product/:id");
-router.post("/productby");
-router.post("/search/filters");
+router.post("/product", create);
+router.get("/products/:count", list);
+router.delete("/product/:id", remove);
+router.post("/productby", listby);
+router.post("/search/filters", searchFilters);
 
 module.exports = router;
