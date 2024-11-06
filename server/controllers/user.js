7fbfd0e6fbfb4422c148dbfa9347a9b8c4261755
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-11-06 11:22:00 +0700

    update change user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,5 @@
+const { prisma } = require("../config/prisma");
+
 exports.listUsers = async (req, res) => {
   try {
     res.send("Hello Users");
@@ -9,25 +11,50 @@ exports.listUsers = async (req, res) => {
 
 exports.changeStatus = async (req, res) => {
   try {
+    const { id, enabled } = req.body;
+    console.log(id);
+    console.log(enabled);
+
+    const user = await prisma.user.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        enabled: enabled,
+      },
+    });
+    console.log(user);
     res.send("Hello changeStatus");
   } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
 exports.changeRole = async (req, res) => {
   try {
-    res.send("Hello changeRole");
+    const { id, role } = req.body;
+    const user = await prisma.user.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        role: role,
+      },
+    });
+    console.log(user);
+    res.send("Change role success ");
   } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
 exports.userCart = async (req, res) => {
   try {
+    const { cart } = req.body;
+    console.log(cart);
     res.send("Hello userCart");
   } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
@@ -35,7 +62,7 @@ exports.getUserCart = async (req, res) => {
   try {
     res.send("Hello getUserCart");
   } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
@@ -43,7 +70,7 @@ exports.emptyCart = async (req, res) => {
   try {
     res.send("Hello emptyCart");
   } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
@@ -52,7 +79,7 @@ exports.saveAddress = async (req, res) => {
   try {
     res.send("Hello saveAddress");
   } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
@@ -60,7 +87,7 @@ exports.saveOrder = async (req, res) => {
   try {
     res.send("Hello saveOrder");
   } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
@@ -69,7 +96,7 @@ exports.getOrder = async (req, res) => {
   try {
     res.send("Hello getOrder");
   } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const { authCheck, adminCheck } = require("../middlewares/authChech");
-const { listUsers } = require("../controllers/user");
+const {
+  listUsers,
+  changeStatus,
+  changeRole,
+  userCart,
+  getUserCart,
+  saveAddress,
+  saveOrder,
+  getOrder,
+  emptyCart,
+} = require("../controllers/user");
 
 router.get("/users", authCheck, adminCheck, listUsers);
-router.post("/change-status", changeStatus);
-router.post("/change-role");
+router.post("/change-status", authCheck, adminCheck, changeStatus);
+router.post("/change-role", authCheck, adminCheck, changeRole);
 
-router.post("/user/cart");
-router.get("/user/cart");
-router.delete("/user/cart/:id");
+router.post("/user/cart", authCheck, userCart);
+router.get("/user/cart", authCheck, getUserCart);
+router.delete("/user/cart", authCheck, emptyCart);
 
-router.post("/user/address");
+router.post("/user/address", authCheck, saveAddress);
 
-router.post("/user/order");
-router.get("/user/order");
+router.post("/user/order", authCheck, saveOrder);
+router.get("/user/order", authCheck, getOrder);
 
 module.exports = router;
