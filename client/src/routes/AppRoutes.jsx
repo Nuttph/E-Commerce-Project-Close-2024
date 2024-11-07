1862de4634fb1fc8a406e2dab44c12bfc87a721b
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-11-07 21:59:34 +0700

    update outlet of react-router-dom

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -4,14 +4,27 @@ import Shop from '../pages/Shop'
 import Cart from '../pages/Cart'
 import History from '../pages/History'
 import Checkout from '../pages/Checkout'
+import Layout from '../layouts/Layout'
+import Login from '../pages/auth/Login'
+import Register from '../pages/auth/Register'
 
 
 const router = createBrowserRouter([
-    { path: '/',element:<Home/>},
-    { path: '/shop',element:<Shop/>},
-    { path: '/cart',element:<Cart/>},
-    { path: '/history',element:<History />},
-    { path: '/checkout',element:<Checkout />},
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {},
+            { index: true,element:<Home/>},
+            { path: '/shop',element:<Shop/>},
+            { path: '/cart',element:<Cart/>},
+            { path: '/history',element:<History />},
+            { path: '/checkout',element:<Checkout />},
+            { path: '/login',element:<Login />},
+            { path: '/register',element:<Register />},
+        ]
+    },
+    
 ])
 
 const AppRoutes = () => {
