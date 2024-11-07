9eefb4ff89d38ec6900568015bf6f3a21fee6786
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-11-07 21:42:22 +0700

    create pages for users

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import Home from '../pages/Home'
 import Shop from '../pages/Shop'
+import Cart from '../pages/Cart'
+import History from '../pages/History'
 
 
 const router = createBrowserRouter([
     { path: '/',element:<Home/>},
     { path: '/shop',element:<Shop/>},
+    { path: '/cart',element:<Cart/>},
+    { path: '/history',element:<History />},
 ])
 
 
