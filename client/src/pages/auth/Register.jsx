1b6ba485c4ec2c397330ae4a91fe1b02b7cf4a95
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-11-09 09:22:04 +0700

    update

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -8,9 +8,16 @@ const Register = () => {
         Password
         <input className="border" type="text" name="email" />
         <button className="bg-blue-500 rounded-md">Register</button>
+        <div>กรอ
+          <div>asdflkasd
+            duvi
+            <div><div>asdl;'asddiv
+            </div></div>
+          </div>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
